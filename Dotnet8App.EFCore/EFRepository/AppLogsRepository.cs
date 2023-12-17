84b17546d362a5cb9c834b02c30120337cab3944
fuyu <fuyu@sanrong.com.tw>
2023-12-18 00:03:28 +0800

    feat: 加入EFRepository.tt

---
+++ Dotnet8App.EFCore/EFRepository/AppLogsRepository.cs
@@ -0,0 +1,17 @@
+﻿using Dotnet8App.EFCore.EntityTables;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace Dotnet8App.EFCore;
+
+public class AppLogsRepository(ApplicationDbContext context) : Repository<AppLogs>(context), IAppLogsRepository
+{
+
+}
+
+public interface IAppLogsRepository : IRepository<AppLogs>
+{
+
+}
+
